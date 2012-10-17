7a6e33d56b04291958f1766901bbf023bfca1281
Shad Storhaug <shad@shadstorhaug.com>
2012-10-18 04:42:06 +0700

    Updated the FileNameGenerator to build a filename based directly off of the URL rather than using a random GUID.

--- src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/Output/FileNameGenerator.cs
+++ src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/Output/FileNameGenerator.cs
@@ -7,6 +7,8 @@
 namespace ContinuousSeo.W3cValidation.Runner.Output
 {
     using System;
+    using System.IO;
+    using System.Text;
     using ContinuousSeo.Core;
 
     /// <summary>
@@ -28,11 +30,68 @@ namespace ContinuousSeo.W3cValidation.Runner.Output
 
         public string GenerateFileName(string url, string extension)
         {
+            if (string.IsNullOrEmpty(url))
+                throw new ArgumentNullException("url");
+            if (string.IsNullOrEmpty(extension))
+                throw new ArgumentNullException("extension");
+
+            const int fileNameLengthLimit = 250;
+            int limit = ((fileNameLengthLimit - extension.Length) - 6); // 1 char for the . and 5 for a random number
+
+            int max = url.Length > limit ? limit : url.Length;
+            char[] chars = url.ToCharArray(0, max);
+            var builder = new StringBuilder();
+            foreach (char c in chars)
+            {
+                if (IsValidFileNameCharacter(c))
+                {
+                    builder.Append(c);
+                }
+                else
+                {
+                    builder.Append('_');
+                }
+            }
+
+            if (builder.Length == limit)
+            {
+                // we have reached the maximum, so we need to add some random numbers to the end to
+                // help ensure uniqueness.
+                var r = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    builder.Append(r.Next(0, 9));
+                }
+            }
+
+            // Now append the extension.
+            builder.Append('.');
+            builder.Append(extension);
+
+            return builder.ToString();
+
             // For now, we are just returning a random Guid for the filename
             // The url is being passed so it can somehow be used to make a more sensible choice in the future.
-            return GuidProvider.NewGuid.ToString().ToLowerInvariant() + "." + extension;
+            //return GuidProvider.NewGuid.ToString().ToLowerInvariant() + "." + extension;
         }
 
         #endregion
+
+        #region Private Methods
+
+        private bool IsValidFileNameCharacter(char input)
+        {
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                if (c.Equals(input))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
     }
 }
