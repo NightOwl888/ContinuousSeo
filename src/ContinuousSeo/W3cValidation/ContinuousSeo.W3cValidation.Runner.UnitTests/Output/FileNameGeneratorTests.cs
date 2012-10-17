eb41d8f3274264784fab173347e1d90beb0fe01b
Shad Storhaug <shad@shadstorhaug.com>
2012-10-18 04:53:19 +0700

    Removed GuidProvider from the FileNameGenerator as it is no longer required.

--- src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner.UnitTests/Output/FileNameGeneratorTests.cs
+++ src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner.UnitTests/Output/FileNameGeneratorTests.cs
@@ -23,28 +23,28 @@ namespace ContinuousSeo.W3cValidation.Runner.UnitTests.Output
 
         #region GenerateFileName Method
 
-        [Test]
-        public void GenerateFileName_ValidUrlAndExtension_ShouldReturnGuidAndExtension()
-        {
-            // arrange
-            var url = "http://www.google.com/test.aspx";
-            var guid = new Guid("e51753ef-d00f-4336-8271-3e30c0017829");
-            var extension = "html";
+        //[Test]
+        //public void GenerateFileName_ValidUrlAndExtension_ShouldReturnGuidAndExtension()
+        //{
+        //    // arrange
+        //    var url = "http://www.google.com/test.aspx";
+        //    var guid = new Guid("e51753ef-d00f-4336-8271-3e30c0017829");
+        //    var extension = "html";
 
-            var guidProvider = new Mock<GuidProvider>();
-            guidProvider.Setup(x => x.NewGuid).Returns(guid);
+        //    var guidProvider = new Mock<GuidProvider>();
+        //    guidProvider.Setup(x => x.NewGuid).Returns(guid);
 
-            FileNameGenerator target = new FileNameGenerator(guidProvider.Object);
+        //    FileNameGenerator target = new FileNameGenerator(guidProvider.Object);
 
-            // act
-            var result = target.GenerateFileName(url, extension);
+        //    // act
+        //    var result = target.GenerateFileName(url, extension);
 
-            // assert
-            var actual = result;
-            var expected = guid.ToString() + "." + extension;
+        //    // assert
+        //    var actual = result;
+        //    var expected = guid.ToString() + "." + extension;
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
 
         #endregion
     }
--- src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/Output/FileNameGenerator.cs
+++ src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/Output/FileNameGenerator.cs
@@ -16,16 +16,6 @@ namespace ContinuousSeo.W3cValidation.Runner.Output
     /// </summary>
     public class FileNameGenerator : IFileNameGenerator
     {
-        private readonly GuidProvider GuidProvider;
-
-        public FileNameGenerator(GuidProvider guidProvider)
-        {
-            if (guidProvider == null)
-                throw new ArgumentNullException("guidProvider");
-
-            this.GuidProvider = guidProvider;
-        }
-
         #region IFileNameGenerator Members
 
         public string GenerateFileName(string url, string extension)
@@ -69,10 +59,6 @@ namespace ContinuousSeo.W3cValidation.Runner.Output
             builder.Append(extension);
 
             return builder.ToString();
-
-            // For now, we are just returning a random Guid for the filename
-            // The url is being passed so it can somehow be used to make a more sensible choice in the future.
-            //return GuidProvider.NewGuid.ToString().ToLowerInvariant() + "." + extension;
         }
 
         #endregion
