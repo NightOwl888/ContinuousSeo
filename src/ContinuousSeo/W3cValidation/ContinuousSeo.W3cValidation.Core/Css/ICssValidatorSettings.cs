eb7c51a8810ef76b0a88534eed4259d67703e82c
Shad Storhaug <shad@shadstorhaug.com>
2012-10-17 21:05:51 +0700

    Updated CssValidator with classes for parsing the soap response.

---
+++ src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Core/Css/ICssValidatorSettings.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ICssValidatorSettings.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ContinuousSeo.W3cValidation.Core.Css
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface ICssValidatorSettings
+    {
+        string UserMedium { get; set; }
+        string CssProfile { get; set; }
+        string Language { get; set; }
+        string WarningLevel { get; set; }
+    }
+}
