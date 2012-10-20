7426b9e36eff16e7680d4249697559cb851da729
Shad Storhaug <shad@shadstorhaug.com>
2012-10-20 14:34:05 +0700

    Refactored the validator DI registry classes.

--- src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/DI/HtmlValidatorRegistry.cs
+++ src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/DI/HtmlValidatorRegistry.cs
@@ -9,88 +9,34 @@ namespace ContinuousSeo.W3cValidation.Runner.DI
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using StructureMap.Configuration.DSL;
-    using ContinuousSeo.Core;
-    using ContinuousSeo.Core.IO;
-    using ContinuousSeo.Core.Net;
-    using ContinuousSeo.Core.Announcers;
     using ContinuousSeo.W3cValidation.Core;
     using ContinuousSeo.W3cValidation.Core.Html;
     using ContinuousSeo.W3cValidation.Runner.Processors;
-    using ContinuousSeo.W3cValidation.Runner.Parsers;
-    using ContinuousSeo.W3cValidation.Runner.Output;
-    using ContinuousSeo.W3cValidation.Runner.Initialization;
+    using ContinuousSeo.W3cValidation.Runner.Xsl;
 
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
-    public class HtmlValidatorRegistry : Registry
+    public class HtmlValidatorRegistry : ValidatorRegistry
     {
         public HtmlValidatorRegistry()
+            : base()
         {
-
-            this.Scan(x =>
-                {
-                    x.LookForRegistries();
-                    x.AssemblyContainingType<ContinuousSeo.Core.DefaultTimeProvider>();
-                    x.AssemblyContainingType<ContinuousSeo.W3cValidation.Core.ResourceCopier>();
-                });
-
-            // HtmlValidationRunnerContext
-            this.For<IAnnouncer>().Use(x => x.GetInstance<ConsoleAnnouncer>());
-
             this.For<IValidatorRunner>().Use(x => x.GetInstance<HtmlValidatorRunner>());
 
-            // HtmlValidationRunner
-            //this.For<IHtmlValidatorRunnerContext>().Use(x => x.GetInstance<HtmlValidatorRunnerContext>());
-            this.For<IUrlProcessor>().Use(x => x.GetInstance<HtmlValidatorUrlProcessor>());
-
-            // HtmlValidationUrlProcessor
+            // HtmlOutputUrlProcessor
             this.For<IValidatorWrapper>().Use(x => x.GetInstance<HtmlValidatorWrapper>());
-            //this.For<IHtmlValidatorRunnerContext>().Use(x => x.GetInstance<HtmlValidatorRunnerContext>()); // TODO: make singleton scope
-            this.For<IUrlAggregator>().Use(x => x.GetInstance<UrlAggregator>());
-            this.For<IFileNameGenerator>().Use(x => x.GetInstance<FileNameGenerator>());
+
             this.For<ResourceCopier>().Use(x => x.GetInstance<HtmlValidatorResourceCopier>()); // From validation.core
-            this.For<IValidatorReportWriterFactory>().Use(x => x.GetInstance<ValidatorReportWriterFactory>());
-            this.For<IStreamFactory>().Use(x => x.GetInstance<StreamFactory>()); // from core
-            this.For<IXslTransformer>().Use(x => x.GetInstance<XslTransformer>());
 
-            // AnnouncingHtmlOutputUrlProcessor
-            
+            this.For<IXslResourceProvider>().Use(x => x.GetInstance<HtmlXslResourceProvider>());
 
-            // 
-            //private readonly AnnouncingXmlOutputUrlProcessor mXmlProcessor;
-        //private readonly AnnouncingHtmlOutputUrlProcessor mHtmlProcessor;
-            this.For<AnnouncingXmlOutputUrlProcessor>().Use(x => x.GetInstance<AnnouncingXmlOutputUrlProcessor>());
-            this.For<AnnouncingHtmlOutputUrlProcessor>().Use(x => x.GetInstance<AnnouncingHtmlOutputUrlProcessor>());
-
-            //this.For<UrlProcessor>().Use<UrlProcessor>()
-            //    .Ctor<UrlProcessor>
-
-            this.For<IUrlProcessorFactory>().Use<UrlProcessorFactory>()
-                .Ctor<UrlProcessor>("xmlProcessor").Is<AnnouncingXmlOutputUrlProcessor>()
-                .Ctor<UrlProcessor>("htmlProcessor").Is<AnnouncingHtmlOutputUrlProcessor>();
-                
-
-            // HtmlValidationWrapper
-            //this.For<HtmlValidator>().Use(x => x.GetInstance<HtmlValidator>()); // from validation.core
+            // HtmlValidatorWrapper
             this.For<HtmlValidator>().Use(x => new HtmlValidator());
-
-            // UrlAggregator
-            this.For<ISitemapsParser>().Use(x => x.GetInstance<SitemapsParser>());
-            this.For<IProjectFileParser>().Use(x => x.GetInstance<ProjectFileParser>());
-
-            // FileNameGenerator
-            //this.For<GuidProvider>().Use(x => x.GetInstance<GuidProvider>());
-            //this.For<GuidProvider>().Use(x => GuidProvider.Current);
 
             // ValidatorReportWriterFactory
             this.For<IValidatorSoap12ResponseParser>().Use(x => x.GetInstance<HtmlValidatorSoap12ResponseParser>());
-
-            // Sitemaps Parser
-            this.For<IHttpClient>().Use(x => x.GetInstance<HttpClient>());
-
         }
     }
 }
---
+++ src/ContinuousSeo/W3cValidation/ContinuousSeo.W3cValidation.Runner/DI/ValidatorRegistry.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ValidatorRegistry.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ContinuousSeo.W3cValidation.Runner.DI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using StructureMap.Configuration.DSL;
+    using ContinuousSeo.Core.Announcers;
+    using ContinuousSeo.Core.IO;
+    using ContinuousSeo.Core.Net;
+    using ContinuousSeo.W3cValidation.Runner.Processors;
+    using ContinuousSeo.W3cValidation.Runner.Output;
+    using ContinuousSeo.W3cValidation.Runner.Parsers;
+    using ContinuousSeo.W3cValidation.Runner.Xsl;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public abstract class ValidatorRegistry : Registry
+    {
+        public ValidatorRegistry()
+        {
+            this.Scan(x =>
+            {
+                x.AssemblyContainingType<ContinuousSeo.Core.DefaultTimeProvider>();
+                x.AssemblyContainingType<ContinuousSeo.W3cValidation.Core.ResourceCopier>();
+            });
+
+            this.For<IAnnouncer>().Use(x => x.GetInstance<ConsoleAnnouncer>());
+            this.For<IUrlAggregator>().Use(x => x.GetInstance<AnnouncingUrlAggregator>());
+            this.For<IFileNameGenerator>().Use(x => x.GetInstance<FileNameGenerator>());
+            this.For<IValidatorReportWriterFactory>().Use(x => x.GetInstance<ValidatorReportWriterFactory>());
+            this.For<IStreamFactory>().Use(x => x.GetInstance<StreamFactory>()); // from core
+            this.For<IXslTransformer>().Use(x => x.GetInstance<XslTransformer>());
+            this.For<IUrlProcessorFactory>().Use<UrlProcessorFactory>()
+                .Ctor<UrlProcessor>("xmlProcessor").Is<AnnouncingXmlOutputUrlProcessor>()
+                .Ctor<UrlProcessor>("htmlProcessor").Is<AnnouncingHtmlOutputUrlProcessor>();
+
+            // UrlAggregator
+            this.For<ISitemapsParser>().Use(x => x.GetInstance<SitemapsParser>());
+            this.For<IProjectFileParser>().Use(x => x.GetInstance<ProjectFileParser>());
+
+            // Sitemaps Parser
+            this.For<IHttpClient>().Use(x => x.GetInstance<HttpClient>());
+        }
+    }
+}
